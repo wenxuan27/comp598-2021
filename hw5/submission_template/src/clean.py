213a38988028d3244b545c07f48efbb7308a3910
wenxuan27 <40556700+wenxuan27@users.noreply.github.com>
2021-11-01 02:59:26 -0400

    updated files

--- hw5/submission_template/src/clean.py
+++ hw5/submission_template/src/clean.py
@@ -1,103 +1,152 @@
 import sys
 import json
-
-from datetime import datetime
-
-
-if len(sys.argv) >= 3:
-    if sys.argv[1] == '-i':
-        input_file = sys.argv[2]
-else:
-    input_file = 'data/example.json'
+import datetime
 
 
-if len(sys.argv) >= 5:
-    if sys.argv[3] == '-o':
-        output_file = sys.argv[4]
-else:
-    output_file = input_file + '.clean'
 
-json.dump({}, open(output_file, 'w'))
 
-with open(input_file) as file:
-    i = 0
-    for line in file:
-        # print(line.rstrip())
 
-        new_json_data = {}
+def validate_author_exists(json_data):
+    return 'author' in json_data
 
-        try:
-            json_data = json.loads(line.rstrip())
-        except Exception as e:
-            print('Error:', e)
-            print(line.rstrip())
-            continue
+def validate_author_not_null_or_na(json_data):
+    return not (json_data['author'] == None or json_data['author'] == 'na' or json_data['author'] == 'NA' or json_data['author'] == 'N/A' or json_data['author'] == 'n/a')
 
-        if not ('author' in json_data):
-            continue
+def validate_datetime_exists(json_data):
+    return 'createdAt' in json_data
 
-        if json_data['author'] == None or json_data['author'] == 'na' or json_data['author'] == 'NA' or json_data['author'] == 'N/A' or json_data['author'] == 'n/a':
-            continue
-        else:
+def validate_title_or_title_text_exists(json_data):
+    return 'title' in json_data or 'title_text' in json_data
+
+def validate_total_count_exists(json_data):
+    return 'total_count' in json_data
+
+
+def load_json(input_str):
+    try:
+        json_data = json.loads(input_str)
+    except Exception as e:
+        return None
+    
+    return json_data
+
+
+def validate_and_transform(json_data):
+
+    if json_data == None:
+        return None
+
+    new_json_data = {}
+    if (validate_author_exists(json_data)):
+        if validate_author_not_null_or_na(json_data):
             new_json_data['author'] = json_data['author']
+        else:
+            return None
 
-        if 'createdAt' in json_data:
+
+    if validate_datetime_exists(json_data):
+        timeiscorrect = True
+        try:
+            t = datetime.datetime.strptime(json_data['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
+            utc_tz = datetime.timezone(datetime.timedelta(seconds=0))
+            new_json_data['createdAt'] = (t.astimezone(utc_tz)).isoformat()
+        except Exception as e:
+            timeiscorrect = False
+
+        if timeiscorrect == False:
             try:
-                datetime.strptime(json_data['createdAt'], '%Y-%m-%dT%H:%M:%S%z')
-                new_json_data['createdAt'] = json_data['createdAt']
+                t = datetime.datetime.fromisoformat(json_data['createdAt'])
+                utc_tz = datetime.timezone(datetime.timedelta(seconds=0))
+                new_json_data['createdAt'] = (t.astimezone(utc_tz)).isoformat()
+
             except Exception as e:
-                continue
-            # try:
-            #     new_json_data['createdAt'] = datetime.fromisoformat(json_data['createdAt'])
-                # new_json_data['createdAt'] = json_data['createdAt']
-            # except Exception as e:
-            #     continue
-        else:
-            continue
+                return None
 
-        if 'total_count' in json_data:
-            if type(json_data['total_count']) == int:
-                new_json_data['total_count'] = json_data['total_count']
-            elif type(json_data['total_count']) == str:
-                try:
-                    new_json_data['total_count'] = int(json_data['total_count'])
-                except Exception as e:
-                    continue
-            elif type(json_data['total_count']) == float:
-                try:
-                    new_json_data['total_count'] = round(json_data['total_count'])
-                except Exception as e:
-                    continue
+
+    if validate_total_count_exists(json_data):
+        if type(json_data['total_count']) == int:
+            new_json_data['total_count'] = json_data['total_count']
+        elif type(json_data['total_count']) == str:
+            try:
+                new_json_data['total_count'] = int(json_data['total_count'])
+            except Exception as e:
+                return None
+        elif type(json_data['total_count']) == float:
+            try:
+                new_json_data['total_count'] = round(json_data['total_count'])
+            except Exception as e:
+                return None
+        else:
+            return None
+
+
+
+    if 'title' in json_data:
+        new_json_data['title'] = json_data['title']
+    elif 'title_text' in json_data:
+        new_json_data['title'] = json_data['title_text']
+    else:
+        return None
+
+    
+    if 'tags' in json_data:
+        new_tags = []
+
+        for tag in json_data['tags']:
+            splitted_tag = tag.split()
+            if len(splitted_tag) == 1:
+                new_tags.append(tag)
             else:
+                for word in splitted_tag:
+                    if word != "" and word != " ":
+                        new_tags.append(word)
+        new_json_data['tags'] = new_tags
+
+
+    if 'text' in json_data:
+        new_json_data['text'] = json_data['text']
+    
+    return new_json_data
+    
+
+def main():
+
+    if len(sys.argv) >= 3:
+        if sys.argv[1] == '-i':
+            input_file = sys.argv[2]
+    else:
+        input_file = 'data/example.json'
+        # input_file = 'test/fixtures/test_6.json'
+
+
+    if len(sys.argv) >= 5:
+        if sys.argv[3] == '-o':
+            output_file = sys.argv[4]
+    else:
+        output_file = input_file + '.clean'
+
+
+    with open(output_file, 'w') as the_file:
+        the_file.write('')
+
+
+    with open(input_file) as file:
+        for line in file:
+            json_data = load_json(line.rstrip())
+ 
+            if(json_data == None):
                 continue
-        else:
-            continue
 
+            new_json_data = validate_and_transform(json_data)
 
-        if 'title' in json_data:
-            new_json_data['title'] = json_data['title']
-        elif 'title_text' in json_data:
-            new_json_data['title'] = json_data['title_text']
-        else:
-            continue
-
+            if new_json_data == {}:
+                continue
+            elif new_json_data == None:
+                continue
+                    
+            with open(output_file, 'a') as the_file:
+                the_file.write(json.dumps(new_json_data)+'\n')
         
-        if 'tags' in json_data:
-            new_tags = []
 
-            for tag in json_data['tags']:
-                splitted_tag = tag.split()
-                if len(splitted_tag) == 1:
-                    new_tags.append(tag)
-                else:
-                    for word in splitted_tag:
-                        if word != "" and word != " ":
-                            new_tags.append(word)
-            new_json_data['tags'] = new_tags
-
-                
-        json.dump(new_json_data, open(output_file, 'a'))
-
-        # print(i, json_data)
-
-        # i += 1
+if __name__ == '__main__':
+    main()
--- hw5/submission_template/test/test_clean.py
+++ hw5/submission_template/test/test_clean.py
@@ -4,14 +4,110 @@ import os, sys
 parentdir = Path(__file__).parents[1]
 sys.path.append(parentdir)
 
+import json
+
+
+from src.clean import *
+
 
 class CleanTest(unittest.TestCase):
-    # def setUp(self):
+    def setUp(self):
         # You might want to load the fixture files as variables, and test your code against them. Check the fixtures folder.
+        # print("setting up...")
+
+        self.test_dir_path = "test/fixtures/"
+
         
 
-    # def test_title(self):
-        # Just an idea for a test; write your implementation
+    def test_title(self):
+        with open(self.test_dir_path + "test_1.json") as file:
+            print("Test 1: ")
+            # No title or title_text and thus the output should be None
+            for line in file:
+                json_data = load_json(line.rstrip())
+                t_json = validate_and_transform(json_data)
+                # print(t_json)
+                message = """Posts that don’t have either "title" or "title_text" should be removed."""
+                self.assertEqual(t_json, None, message)
+            
+            print("OK")
+
+    def test2(self):
+        with open(self.test_dir_path + "test_2.json") as file:
+            print("test 2:")
+            # datetime in createdAt doesn't match the format and thus output should be None
+            for line in file:
+                json_data = load_json(line.rstrip())
+                message = "createdAt dates that don’t pass the ISO datetime standard should be removed."
+                self.assertEqual(validate_datetime_exists(json_data), True, message)
+                t_json = validate_and_transform(json_data)
+
+                # print(t_json)
+                self.assertEqual(t_json, None, message)
+
+            print("OK")
+
+    def test3(self):
+        with open(self.test_dir_path + "test_3.json") as file:
+
+            print("test 3:")
+            # Invalid JSON dictionary so output should be None
+            for line in file:
+                json_data = load_json(line.rstrip())
+                message = "Any lines that contain invalid JSON dictionaries should be ignored."
+                self.assertEqual(json_data, None, message)
+            
+            print("OK")
+
+    def test4(self):
+        with open(self.test_dir_path + "test_4.json") as file:
+            print("test 4:")
+            # author is null, N/A, or empty string and thus output should be None
+            for line in file:
+                json_data = load_json(line.rstrip())
+
+                self.assertEqual(validate_author_not_null_or_na(json_data), False, "author is not null or NA")
+                t_json = validate_and_transform(json_data)
+                message = """Any lines for which "author" is null, N/A or empty."""
+                self.assertEqual(t_json, None, message)
+            
+            print("OK")
+    
+    def test5(self):
+        with open(self.test_dir_path + "test_5.json") as file:
+            print("test 5:")
+            # total_count is not cast-able to an int and thus output should be None
+            for line in file:
+                json_data = load_json(line.rstrip())
+                t_json = validate_and_transform(json_data)
+                message = """total_count is a string containing a cast-able number, total_count is cast to an int properly."""
+                # print(t_json)
+                self.assertEqual(t_json, None, message)
+            
+            print("OK")
+
+    def test6(self):
+        with open(self.test_dir_path + "test_6.json") as file:
+            print("test 6:")
+            # tags should be a list of words with each containing no spaces
+            for line in file:
+                json_data = load_json(line.rstrip())
+                # len_tags_before = len(json_data["tags"])
+                t_json = validate_and_transform(json_data)
+
+                # len_tags_after = len(t_json["tags"])
+                message = """The tags field gets split on spaces when given a tag containing THREE words (e.g., “nba basketball game”)."""
+                # print(t_json["tags"])
+                
+                for tag in t_json["tags"]:
+                    splitted = tag.split()
+                    # print(tag, len(tag), splitted, len(splitted))
+                    self.assertEqual(len(splitted), 1, message)
+                
+                print("OK")
+                    
+
+    
         
     
 if __name__ == '__main__':
